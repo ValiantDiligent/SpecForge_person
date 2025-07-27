14c6539a1c82b516a4df06c053d76f1bb4c37d96
zhanghuaxiang <zhanghuaxiang@kuaishou.com>
2025-07-28 01:46:30 +0800

    add

---
+++ scripts/tese.py
@@ -0,0 +1,11 @@
+import requests
+
+url = f"http://localhost:{port}/v1/chat/completions"
+
+data = {
+    "model": "XiaomiMiMo/MiMo-7B-RL",
+    "messages": [{"role": "user", "content": "What is the capital of France?"}],
+}
+
+response = requests.post(url, json=data)
+print_highlight(response.json())
