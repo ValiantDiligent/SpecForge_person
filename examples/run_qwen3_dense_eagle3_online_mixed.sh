b3390ae6c348863ecc057bacf16d9fe40b5c4648
zhanghuaxiang <zhanghuaxiang@kuaishou.com>
2025-08-08 22:39:04 +0800

    1

---
+++ examples/run_qwen3_dense_eagle3_online_mixed.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT_DIR=$(dirname $SCRIPT_DIR)
+
+# support tp8 train eagle3 for Qwen3-4B/8B/32B
+NUM_GPUS=${1:-2}
+
+torchrun \
+    --standalone \
+    --nproc_per_node $NUM_GPUS \
+    $ROOT_DIR/scripts/train_eagle3_online.py \
+    --target-model-path /media/qwen3_32b \
+    --draft-model-config $ROOT_DIR/configs/qwen3-32b-eagle3.json \
+    --train-data-path $ROOT_DIR/cache/dataset/train-mixed-private.json \
+    --output-dir $ROOT_DIR/outputs/Qwen3-32B-eagle3_32b \
+    --num-epochs 5 \
+    --batch-size 4 \
+    --learning-rate 1e-4 \
+    --max-length 2048 \
+    --chat-template qwen3 \
+    --cache-dir $ROOT_DIR/cache_qwen3_32B \
+    --embedding-key model.embed_tokens.weight \
+    --tp-size $NUM_GPUS \
+    --ttt-length 7
